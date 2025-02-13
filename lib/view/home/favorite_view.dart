0cc8d1857f04a581fa3b63f491fa2690f520c808
MarwaAtef <marwaatef2001@gmail.com>
2025-02-13 21:52:37 +0200

    finsh store and favorit view

--- lib/view/home/favorite_view.dart
+++ lib/view/home/favorite_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/view/home/widget/custom_product_card.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteView extends StatelessWidget {
@@ -5,8 +6,26 @@ class FavoriteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('data3'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 16,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Your Favorit Products',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomProductCard(),
+        ],
+      ),
     );
   }
 }
--- lib/view/home/store_view.dart
+++ lib/view/home/store_view.dart
@@ -1,12 +1,39 @@
+import 'package:e_commerce/core/Custom_Search_Field.dart';
 import 'package:flutter/material.dart';
+
+import 'widget/ProductCardListView.dart';
 
 class StoreView extends StatelessWidget {
   const StoreView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('data2'),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'Welcome To Our Market',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomSearchField(),
+            SizedBox(
+              height: 16,
+            ),
+            ProductCardListView(),
+          ],
+        ),
+      ),
     );
   }
 }
