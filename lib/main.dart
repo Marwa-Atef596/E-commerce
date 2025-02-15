a0d1828d93fe44f26a413ae81556cdad08c1755b
MarwaAtef <marwaatef2001@gmail.com>
2025-02-15 17:19:06 +0200

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:e_commerce/core/app_colors.dart';
+import 'package:e_commerce/core/keys.dart';
 import 'package:e_commerce/view/auth/ui/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: Keys.url,
+    anonKey: Keys.anonKey,
+  );
   runApp(const OurMarket());
 }
 
